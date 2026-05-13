9616bced4787d59bac971fa419f5ef32c5d16476
IRA9pro <ira9program@gmail.com>
2026-05-13 14:36:46 +0600

    Supabase Impl.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:pulse/services/supabase_service.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SupabaseService.init();
+
+  runApp(const MaterialApp());
 }
---
+++ lib/services/supabase_service.dart
@@ -0,0 +1,12 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseService {
+  static Future<void> init() async {
+    await Supabase.initialize(
+      url: 'https://kbtlkjcclkquyhiwpaet.supabase.co/rest/v1/',
+      anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtidGxramNjbGtxdXloaXdwYWV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwNzU4NTIsImV4cCI6MjA5MTY1MTg1Mn0.oUZG5QSBpZMdPP-shXF_KnymHOdomVUw4aW6pUMtC8A',
+    );
+  }
+
+  SupabaseClient get client => Supabase.instance.client;
+}
